5a3de1862aade67aefc18cd7b329a2afbb10c81c
Keita Moromizato <keita_moromizato@relationsgroup.co.jp>
2016-11-06 19:41:08 +0900

    fix

---
+++ README.md
@@ -0,0 +1,1 @@
+http://ameblo.jp/ca-1pixel/entry-11815188808.html
--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 const XElementPrototype = Object.create(HTMLElement.prototype);
+
+XElementPrototype.createdCallback = function(){
+
+  const template = document.getElementById('x-element-template');
+  const clone = document.importNode(template.content, true);
+  const shadowRoot = this.createShadowRoot();
+  shadowRoot.appendChild(clone);
+
+  this.addEventListener('click', e => {
+    alert('Click');
+  });
+};
+
+XElementPrototype.attachedCallback = () => {
+  console.log('attached');
+};
+
+XElementPrototype.detachedCallback = () => {
+  console.log('detached');
+};
+
+XElementPrototype.attributeChangedCallback = (attribute, oldValue, newValue) => {
+  console.log('prototype changed', attribute, oldValue, newValue);
+};
+
 const XElement = document.registerElement('x-element', {
-  prototype: XElementPrototype
+  prototype: XElementPrototype,
+  extends: 'button',
 });
 
 const xElement = new XElement();
-xElement.textContent = 'これはXElementです';
+xElement.textContent = '独自のボタン';
 document.body.appendChild(xElement);
